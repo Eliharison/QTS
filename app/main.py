eefb4500c5af2b5ff77707aae72c9f45f6989f0b
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2025-11-22 15:34:02 -0300

    feat: black and flake8 in CI - ATV2-T2 Green

--- app/main.py
+++ app/main.py
@@ -3,16 +3,8 @@ from fastapi import FastAPI, HTTPException, status
 app = FastAPI(title="Fatec299")
 
 estudantes = {
-    1: {
-        "id": 1,
-        "nome": "Eliharison",
-        "idade": 20
-    },
-    2: {
-        "id": 2,
-        "nome": "Rodrigo",
-        "idade": 20
-    }
+    1: {"id": 1, "nome": "Eliharison", "idade": 20},
+    2: {"id": 2, "nome": "Rodrigo", "idade": 20},
 }
 
 
@@ -24,7 +16,5 @@ def buscar_estudante_por_id(estudante_id: int):
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail={
-                "mensagem": "Estudante não encontrado"
-            }
+            detail={"mensagem": "Estudante não encontrado"},
         )
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,18 +7,12 @@ client = TestClient(app)
 def test_buscar_estudante_por_id():
     response = client.get("estudante/1")
     assert response.status_code == 200
-    assert response.json() == {
-        "id": 1,
-        "nome": "Eliharison",
-        "idade": 20
-    }
+    assert response.json() == {"id": 1, "nome": "Eliharison", "idade": 20}
 
 
 def test_buscar_estudante_por_id_inexistente():
     response = client.get("estudante/999")
     assert response.status_code == 404
     assert response.json() == {
-        "detail": {
-            "mensagem": "Estudante não encontrado"
-        }
+        "detail": {"mensagem": "Estudante não encontrado"}
     }
